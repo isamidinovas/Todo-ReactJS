02488e6c5abd147cbdccfe76819b984079151665
isamidinovas <99507186+isamidinovasymbatt@users.noreply.github.com>
2022-04-18 15:27:06 +0600

    (Lesson 2)finished the left menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import List from "./components/List/List/LIst/LIst"
+import List from "./components/List"
 
 function App(){
   
@@ -18,6 +18,7 @@ function App(){
              
                 }
             ]}
+
             />
 
              <List
@@ -35,7 +36,21 @@ function App(){
                     color:'pink',
                   name:'Фильмы и сериалы...'
                     }
-            ]}/>
+            ]}
+            isRemovable={true}
+            />
+          <List
+              items={[
+              {
+                className:"list__add-button",
+                icon: <svg width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8 1V15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M1 8H15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>,
+              name:'Добавить список'
+                },
+            ]}
+            />
 
           </div>
           <div className="todo_tasks"></div>
---
+++ src/components/List/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './list.scss'
+
+
+
+const List = ({ items, isRemovable }) => {
+    console.log(items, isRemovable);
+    return (
+        <ul className="list">
+            {
+                items.map(item => <li className={item.active ? 'active' : ""}>
+                    <i>
+                        {item.icon ? item.icon : <i className={`badge badge--${item.color}`}/>}
+                    </i>
+                    
+                    <span>{item.name}</span>
+                    
+                    
+                </li>)
+            }
+            
+        </ul>
+    );
+};
+
+export default List;
