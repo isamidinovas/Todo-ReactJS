bc0216a7bfaf6544a461fb4a986ed72700d86645
isamidinovas <99507186+isamidinovasymbatt@users.noreply.github.com>
2022-04-21 08:49:38 +0600

    added an add button to the left side of the first page, you can add a list(3 lesson)

---
+++ src/components/AddList/index.jsx
@@ -0,0 +1,69 @@
+import React, {useState} from "react";
+import List from '../List';
+import '../../components/AddList/AddListBotton.scss'
+import Badge from '../Badge';
+import closeSvg from '../../assets/img/close.svg'
+
+const AddListButton =({colors, onAdd}) => {
+  const [visiblePopup, setVisiblePopup]=useState(true);
+  const [seletedColor, selectColor]=useState(colors[0].id);
+  const [inputValue, setInputValue]=useState('')
+
+  const onClose =() => {
+    setVisiblePopup(false);
+    setInputValue('')
+    selectColor(colors[0].id);
+  }
+  const addList = () => {
+    if(!inputValue) {
+      alert("Введите название списка")
+      return;
+    }
+    
+    const color =colors.filter(c => c.id ===seletedColor)[0].name;
+    onAdd({"id":Math.random(), "name": inputValue, color});
+    onClose();
+  };
+    return(
+      <div className="add-list">
+        <List
+        onClick={()=>setVisiblePopup(true)}
+        items={[
+        {
+          className:"list__add-button",
+          icon:<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M8 1V15" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M1 8H15" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          ,
+        name:'Добавить список' 
+          }
+      ]}
+      />
+      { visiblePopup && <
+        div className="add-list__popup">
+        <img onClick={onClose} 
+        src={closeSvg} alt="close btn" className="add-list__popup-close-btn"/>
+        <input value={inputValue} 
+         onChange={e => setInputValue(e.target.value) }
+        className="field" type="text" placeholder="Называние списка" />
+        <div className="add-list__popup-colors">
+
+              {
+                colors.map(color =>( 
+                <Badge onClick={()=> selectColor(color.id)} 
+                key={color.id}
+                 color={color.name}
+                 className={seletedColor=== color.id && 'active'}/>
+                ))}
+
+        </div>
+         <button onClick={addList} className="button">Добавить</button>
+      </div>
+      }
+      </div>
+    );
+  };
+
+
+export default AddListButton;
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import './list.scss'
+import removeSvg from '../../assets/img/remove.svg'
+import Badge from "../Badge";
 
+const List = ({ items, isRemovable, onClick,onRemove }) => {
 
-
-const List = ({ items, isRemovable }) => {
-    console.log(items, isRemovable);
+    const removeList =(item) =>{
+        if(window.confirm('Вы действителбно хотите удалить список?')){
+            onRemove(item);
+        }    }
     return (
-        <ul className="list">
+        <ul onClick={onClick} className="list">
             {
-                items.map(item => <li className={item.active ? 'active' : ""}>
-                    <i>
-                        {item.icon ? item.icon : <i className={`badge badge--${item.color}`}/>}
+                items.map(item=> <li className={item.active ? 'active' : ""}>
+                    <i>{
+                    <Badge color={item.color}/>}
                     </i>
                     
                     <span>{item.name}</span>
                     
-                    
+                 {isRemovable && 
+                 <img className="list__remove-icon"
+                  src={removeSvg} alt="remove icon"
+                  onClick= {() =>removeList(item)}
+                  />}
                 </li>)
             }
             
